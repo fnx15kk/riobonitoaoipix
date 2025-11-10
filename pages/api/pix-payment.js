a2cac06621026fecba4a6022c64da156677c1e73
Victor Gabriel <victor2006gr@gmail.com>
2025-11-10 12:20:43 -0300

    Adiciona componente de checkout com PIX

--- pages/api/pix-payment.js
+++ pages/api/pix-payment.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
 
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { orderId, amount, customerInfo } = req.body;
+
+    // URL da API Mangofy para criar um pagamento via PIX
+    const apiUrl = 'https://api.mangofy.com/checkout/pix';  // Substitua com a URL da Mangofy
+
+    const payload = {
+      client_secret: process.env.25cca9a5795f24cbf2887832f2235780,  // Usando a chave secreta que você configurou na Vercel
+      orderId,
