914731a57e3f8b85fb77c7f0645d74471cc4a7af
Victor Gabriel <victor2006gr@gmail.com>
2025-11-10 12:33:41 -0300

    Create pix-payment.js

---
+++ pages/api/pix-payment.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { orderId, amount, customerInfo } = req.body;
+
+    const apiUrl = 'https://api.mangofy.com/checkout/pix';  // URL da API da Mangofy
+
+    const payload = {
+      client_secret: process.env.25cca9a5795f24cbf2887832f2235780,  // A chave secreta configurada
+      orderId,
+      amount, // O valor da doação
+      customer: customerInfo,  // Informações do cliente
+    };
+
+    try {
+      const response = await axios.post(apiUrl, payload, {
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      const { pixUrl, qrCode } = response.data;
+
+      return res.status(200).json({ pixUrl, qrCode });
+    } catch (error) {
+      console.error('Erro ao gerar pagamento PIX:', error);
+      return res.status(500).json({ error: 'Erro ao gerar pagamento PIX' });
+    }
+  } else {
+    return res.status(405).json({ error: 'Método não permitido' });
+  }
+}
